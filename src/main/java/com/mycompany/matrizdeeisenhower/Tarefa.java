133c8551f1c3205020d73af17fcdb0326b8340a1
LeonardoKarate <leonardokarate95@hotmail.com>
2025-07-03 01:50:08 +0100

    cria a classe Tarefa

--- src/main/java/com/mycompany/matrizdeeisenhower/Tarefa.java
+++ src/main/java/com/mycompany/matrizdeeisenhower/Tarefa.java
@@ -9,5 +9,89 @@ package com.mycompany.matrizdeeisenhower;
  * @author leona
  */
 public class Tarefa {
+    private int id;
+    private String titulo;
+    private String descricao;
+    private boolean urgente;
+    private boolean importante;
+    private boolean feito;
+    
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the urgente
+     */
+    public boolean isUrgente() {
+        return urgente;
+    }
+
+    /**
+     * @param urgente the urgente to set
+     */
+    public void setUrgente(boolean urgente) {
+        this.urgente = urgente;
+    }
+
+    /**
+     * @return the importante
+     */
+    public boolean isImportante() {
+        return importante;
+    }
+
+    /**
+     * @param importante the importante to set
+     */
+    public void setImportante(boolean importante) {
+        this.importante = importante;
+    }
+
+    /**
+     * @return the feito
+     */
+    public boolean isFeito() {
+        return feito;
+    }
+
+    /**
+     * @param feito the feito to set
+     */
+    public void setFeito(boolean feito) {
+        this.feito = feito;
+    }
     
 }
