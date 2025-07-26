df0e39426e52de9b7f54f1d0aed54b887e54ae5b
LeonardoKarate <leonardokarate95@hotmail.com>
2025-07-27 00:01:35 +0100

    adiciona dependencias

--- src/main/java/com/mycompany/matrizdeeisenhower/dao/TarefaDAO.java
+++ src/main/java/com/mycompany/matrizdeeisenhower/dao/TarefaDAO.java
@@ -4,10 +4,78 @@
  */
 package com.mycompany.matrizdeeisenhower.dao;
 
+import java.util.ArrayList;
+import com.mycompany.matrizdeeisenhower.Tarefa;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author leona
  */
 public class TarefaDAO {
+    
+    public ArrayList<Tarefa> minhaLista = new ArrayList<>();
+    
+    public Connection getConexao() {
+        Connection conection = null;
+        try {
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+            
+            String server = "localhost";
+            String database = "db_tarefas";
+            String url = "jdbc:mysql://" + server + ":3306/" + database + "?useTimezone=true&serverTimezone=UTC";
+            String user = "root";
+            String password = "root!";
+            
+            conection =DriverManager.getConnection(url, user, password);
+            
+            if (conection != null) {
+                System.out.println("Conectado");                
+            } else {
+                System.out.println("Não conectado");
+            }
+            
+            return conection;
+            
+        } catch (ClassNotFoundException e) {
+            System.out.println("o drive não foi encontrado" + e.getMessage());
+            return null;
+        }   catch (SQLException e) {
+            System.out.println("não foi possivel conectar");
+            return null;
+        }
+    }
+    
+    public ArrayList<Tarefa> getAll() {
+        
+        minhaLista.clear();
+        
+        try {
+            Statement stmt = this.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_tarefas");
+            
+            while (res.next()) {                
+                int id = res.getInt("id");
+                String descricao = res.getString("descricao");
+                String titulo = res.getString("titulo");
+                boolean importante = res.getBoolean("importante");
+                boolean urgente = res.getBoolean("urgente");
+                boolean feito = res.getBoolean("feito");
+                
+                Tarefa objeto = new Tarefa(id, titulo, descricao, urgente, importante, feito);
+                minhaLista.add(objeto);
+            }
+            stmt.close();
+            
+        } catch (SQLException e) {
+            System.out.println("Erro:" + e);
+        }
+        return minhaLista;
+    }
     
 }
